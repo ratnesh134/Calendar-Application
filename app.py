227e9095d2b73767e0754ce6bdd18e6906818c18
Ratnesh Kumar <ratnesh134@gmail.com>
2025-10-28 23:08:31 +0530

    Application File

---
+++ app.py
@@ -0,0 +1,12 @@
+class Calendar:
+
+    def __init__(self,day,month,year):
+        self.day = day
+        self.month = month
+        self.year = year
+
+    def dayFinder(self):
+        pass
+
+    
+        
